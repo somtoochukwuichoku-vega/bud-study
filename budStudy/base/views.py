da4c4216aca892528a99abaa5b514ee1fbcefedd
Ichoku Somtoochukwu <somtoochukwu.ichoku@vegaitglobal.com>
2026-01-14 13:01:05 +0100

    add templates

--- budStudy/base/views.py
+++ budStudy/base/views.py
@@ -8,7 +8,6 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
-from budStudy.base.models import message
 from .models import Room, Topic, message
 from .forms import RoomForm
 
@@ -113,7 +112,7 @@ def profile(request,pk):
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
     topics = Topic.objects.all()
-    context = {'user':user,'rooms':rooms, 'topics':topics}
+    context = {'user':user,'rooms':rooms, 'topics':topics, 'room_messages':room_messages}
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
@@ -123,7 +122,9 @@ def createRoom(request):
         # print(request.POST)
         form=RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form':form}
--- budStudy/budStudy/urls.py
+++ budStudy/budStudy/urls.py
@@ -21,5 +21,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # web application endpoint
     path('', include('base.urls'))
 ]
